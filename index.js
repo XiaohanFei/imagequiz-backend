c9817c609b61773a5f0fb6f7f74e15849e3922e1
XiaohanFei <gshxd123456@email.arizona.edu>
2021-11-28 22:34:18 -0700

    +

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port =process.env.PORT || 4002;
 application.get('/add/:n/:m',(request,response)=>{
     let n =Number(request.params.n);
     let m=Number(request.params.m);
-    let sum=api.add(n.m)
+    let sum=api.add(n,m)
     response.send(`${n}+${m}=${sum}.`);
 
 })
