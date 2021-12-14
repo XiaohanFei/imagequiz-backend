ab9725a63cc8a0d1225e509830e357254883d409
XiaohanFei <gshxd123456@email.arizona.edu>
2021-12-13 17:08:27 -0700

    done

--- index.js
+++ index.js
@@ -114,9 +114,9 @@ application.post('/login', (request, response) => {
     api.customerLogin(email, password)
     .then(x=>{
         if(x) {
-            response.json({message: 'Login successful'});
+            response.json({message: 'Login successful', done:true});
         } else {
-            response.status(404).json({message: 'Login not successful'});
+            response.status(404).json({message: 'Login not successful',done:false});
         }
     }
     ) 
